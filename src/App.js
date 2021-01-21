a5f2b9d1f028242d889b81d356db8157aec4b39e
rjworks <arjayandal05@yahoo.com>
2021-01-21 14:03:39 -0600

    Add user-instagram, input, and fetch raw data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
 import 'fomantic-ui/dist/semantic.min.css'
+import Home from "./components/Home";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <div className="ui scrolling local search">
-                    <div className="ui icon input">
-                        <form>
-                            <label>
-                                Name:
-                                <input type="text" name="name" />
-                            </label>
-                            <input type="submit" value="Submit" onSubmit={(e) => {
-                                console.log(e.target.value)
-                            }}/>
-                        </form>
-                        <i className="search icon"></i>
-                    </div>
-                </div>
+                <Home/>
             </header>
         </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,46 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import userInstagram from 'user-instagram'
 const MyComponent = () => {
+
+    const [text, setText] = useState( null );
+
+    const onChange = event => {
+        setText(event.target.value)
+    }
+
+    const onClick = () => {
+        userInstagram(text) // Same as getUserData()
+            .then(console.log)
+            .catch(console.error);
+    }
+
     return (
         <div>
+            <div className="searchBar">
+                <div className="ui action input">
+                    <input
+                        type="text"
+                        placeholder="Instagram @"
+                        autoComplete="off"
+                        autoFocus="on"
+                        style={{
+                            fontSize: '25px'
+                        }}
+                        onChange={onChange}
+                    />
+                    <button
+                        className="ui icon button"
+                        onClick={onClick}
+                    >
+                        <i
+                            className="search icon"
+                            style={{
+                                width: '50px'
+                            }}
+                        />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
